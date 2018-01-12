4cf1437acc1f20b26deb1bb3f071cd56ee8371c3
Ricky Hammonds <mail@rhammonds.com>
2018-01-12 12:58:51 -0500

    Add files via upload

--- Angular2Katas/Dialog/src/app/components/customer.component.ts
+++ Angular2Katas/Dialog/src/app/components/customer.component.ts
@@ -9,6 +9,9 @@ export class CustomerComponent {
     constructor(
         private router: Router,
     ) {}
+    public customerName = 'George Washington';
+    public customerAddress = '123 Apple Street, Atlanta, GA 12345';
+
     viewHome()    {
         this.router.navigate(['home']);
     }
